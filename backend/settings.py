dae7d671429f03d010e03618e4d9eef518504cf1
bssaranya <bssaranya2919@gmail.com>
2024-06-21 20:42:53 +0530

    update staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # make sure to add this line
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
